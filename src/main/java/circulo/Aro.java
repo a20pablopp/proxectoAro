692e422e86761239254ca5b214b62fe4d1dd578c
Pablo Perez Paz <a20pablopp@iessanclemente.net>
2022-03-09 20:13:59 +0100

    Renombrada costante MINIMO a LIMITERADIO con valor 0.0 tipo double.

--- src/main/java/circulo/Aro.java
+++ src/main/java/circulo/Aro.java
@@ -5,7 +5,7 @@ package circulo;
  * @author Pablo Perez Paz
  */
 public class Aro {
-    public static final double MINIMO = 0.0;
+    public static final double LIMITERADIO = 0.0;
 
     private int coordenadaX;
     private int coordenadaY;
@@ -38,7 +38,7 @@ public class Aro {
 
     public void establecerRadio(double valorRadio) {
 
-        radio=(valorRadio < MINIMO ? MINIMO : valorRadio);
+        radio=(valorRadio < LIMITERADIO ? LIMITERADIO : valorRadio);
     }
 
     public double obterRadio() {
